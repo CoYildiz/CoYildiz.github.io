5fcde51d25e7dce5a1d0bb1aa091ebf850d90efd
CoYildiz <127496796+CoYildiz@users.noreply.github.com>
2026-06-01 02:08:38 +0300

    restructure the blog,about

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/ds.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Yusuf",
-	bio: "idk",
+	bio: "Exploring software, ideas, books, games, and everything that sparks curiosity.",
 	links: [
 		
 			// You will need to install the corresponding icon set if it's not already included
